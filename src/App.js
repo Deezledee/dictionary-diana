610a9122cb57c0dfb7bb6ba09319342e27bb3314
Diana <dianakalstein@gmail.com>
2022-03-15 10:20:13 +0100

    Correcting App.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import logo from "./logo.png";
-
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
-
-export default App;
